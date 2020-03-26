e7b32c1466b13bf141dedc6aea19087f4a4fe46f
Tetsumote <unionproject88@gmail.com>
2020-03-26 18:30:57 +0100

    add fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,33 @@
-import Layout from '../comps/MyLayout'
-import Link from 'next/link'
+import Layout from '../comps/MyLayout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
 
-
-const PostLink = props => (
-<li>
-    <Link href={`/post?title=${props.title}`}>
-        <a>{props.title}</a>
-    </Link>
-</li>
+const Index = props => (
+    <Layout>
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {props.shows.map(show => (
+                <li key={`${show.id}`}>
+                    <Link href="/p/[id]" as={`/p/${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </Layout>
 )
 
+Index.getInitialProps = async function(){
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
 
-export default function Blog(){
-    return(
-        <Layout>
-            <h1>My blog</h1>
-            <ul>
-                <PostLink title="Hello Next.js" />
-                <PostLink title="Learn Next.js is awesome" />
-                <PostLink title="Deploy apps with Zeit" />
-            </ul>
-        </Layout>
-    )
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return{
+        shows:data.map(entry => entry.show)
+    }
+
 }
+
+export default Index;
