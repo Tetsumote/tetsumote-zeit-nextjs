2ef042b7600224935d14c4ffe4d77b6082d91955
Tetsumote <unionproject88@gmail.com>
2020-03-26 18:48:11 +0100

    add styles

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,53 @@
 import Layout from '../comps/MyLayout'
+import Link from 'next/link';
+
+function getPosts(){
+    return [
+        {id: 'hello-nextjs', title:'Hello Next.js'},
+        {id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
+        {id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
+    ]
+}
 
 
 export default function About(){
     return(
         <Layout>
             <p>This is about page</p>
+            <p>My Blog</p>
+            <ul>
+                {getPosts().map(post => (
+                    <li key={`${post.id}`}>
+                        <a href="/my-blog/[id]" as={`/p/${post.id}`}>{post.title}</a>
+                    </li>
+                ))}
+            </ul>
+            <style jsx>
+                {`
+                    h1,
+                    a {
+                      font-family: 'Arial';
+                    }
+            
+                    ul {
+                      padding: 0;
+                    }
+            
+                    li {
+                      list-style: none;
+                      margin: 5px 0;
+                    }
+            
+                    a {
+                      text-decoration: none;
+                      color: blue;
+                    }
+            
+                    a:hover {
+                      opacity: 0.6;
+                    }
+                `}
+            </style>
         </Layout>
     )
 }
---
+++ pages/my-blog/[id].js
@@ -0,0 +1,11 @@
+import Layout from '../../comps/MyLayout';
+
+const Post = props => (
+  <Layout>
+    <h1>{props.id}</h1>
+  </Layout>
+);
+
+
+
+export default Post;
